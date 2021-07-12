9390604c96f9380d36bbb589e1bf695ce990a1b8
Glazzerino <fztmon@gmail.com>
2021-07-12 12:53:24 -0500

    blank app

---
+++ app/src/main/java/com/fbu/autonote/models/Note.java
@@ -0,0 +1,13 @@
+package com.fbu.autonote.models;
+
+import java.util.List;
+
+public class Note {
+    public String topic;
+    public List<String> keywords;
+    public String ImageURL;
+    public String textContent;
+    public long noteId;
+    public long userId;
+    public long collectionId;
+}
